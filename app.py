f05a1c6999cd34a870b2ebd4850d3172e2ee2bb5
aashika03 <ashika.lamichhane.10@gmail.com>
2025-04-18 22:46:50 +0545

    first commit

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask, redirect, request,jsonify, session
+import requests, os
+from urllib.parse import urlencode
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+REDIRECT_URI = os.getenv("SPOTIFY_REDIRECT_URI")
+app.secret_key = os.getenv("FLASK_SECRET_KEY", "default_unsafe_key")
+
+
+AUTH_URL = "https://accounts.spotify.com/authorize"
+TOKEN_URL = "https://accounts.spotify.com/api/token"
+SCOPE = "user-read-private user-read-email"
+
+@app.route('/')
+def login():
+    query = urlencode({
+        "client_id": CLIENT_ID,
+        "response_type": "code",
+        "redirect_uri": REDIRECT_URI,
+        "scope": SCOPE,
+    })
+    return f'<a href="{AUTH_URL}?{query}">Log in with Spotify</a>'
+
+@app.route('/callback')
+def callback():
+    code = request.args.get("code")
+    
+    res = requests.post(
+        TOKEN_URL,
+        data={
+            "grant_type": "authorization_code",
+            "code": code,
+            "redirect_uri": REDIRECT_URI,
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+        },
+    )
+
+    token_data = res.json()
+    access_token = token_data.get("access_token")
+
+    session["access_token"] = access_token 
+    
+    return f"<p>Access Token:</p><pre>{access_token}</pre>"
+
+@app.route('/profile')
+def profile():
+    access_token= session.get("access_token")
+    if not access_token:
+        return redirect("/")
+    headers = {
+        "Authorization": f"Bearer {access_token}"
+    }
+    response = requests.get("https://api.spotify.com/v1/me", headers=headers)
+    data = response.json()
+
+    return jsonify(data)
+
+@app.route('/crazy')
+def crazy():
+    return ("<p>Crazy</p><h1>I was cray once, but now I am sane.</h1>")
+    
+if __name__ == '__main__':
+    app.run(port=3000)
+
